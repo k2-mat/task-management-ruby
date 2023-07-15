a5247954b8f9c556654566eefd83476a9bac83a7
k2-mat <k.matsu9@gmail.com>
2023-07-15 10:29:27 +0900

    bundle update

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["body", "created_at", "id", "title", "updated_at", "user_id"]
+  end
 end
